7bec2ee04121ae5eafedd4aae4772425da946ef3
Eren <erencan_arica@hotmail.com>
2024-07-08 15:02:20 +0200

    Added @vercel/analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,8 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
-      <Toaster />
+      <body
+        className={inter.className}>
+        {children}
+        <Analytics />
+        <Toaster />
+      </body>
     </html>
   );
 }
