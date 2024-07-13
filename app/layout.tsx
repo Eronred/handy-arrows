77522e1decfc44c493e5c912548bc822d9a421e0
Eren <erencan_arica@hotmail.com>
2024-07-13 14:57:12 +0200

    chore: Update layout.tsx to lazy load Umami analytics script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "@/components/ui/toaster"
-import { Analytics } from "@vercel/analytics/react"
+import { Toaster } from "@/components/ui/toaster";
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,13 +16,20 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const umamiWebsiteId = process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID;
+
   return (
     <html lang="en">
-      <body
-        className={inter.className}>
+      <body className={inter.className}>
         {children}
-        <Analytics />
         <Toaster />
+        {umamiWebsiteId && (
+          <Script
+            src="https://cloud.umami.is/script.js"
+            data-website-id={umamiWebsiteId}
+            strategy="lazyOnload"
+          />
+        )}
       </body>
     </html>
   );
