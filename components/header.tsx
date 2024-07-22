c30db5fc7b24f12a5e14e757a80caa45f724bc31
Eren <erencan_arica@hotmail.com>
2024-07-22 13:48:03 +0300

    chore: Update header component with license and contact links

--- components/header.tsx
+++ components/header.tsx
@@ -1,8 +1,8 @@
-import { Github, Twitter, X } from 'lucide-react'
-import Head from 'next/head'
-import Link from 'next/link'
-import React from 'react'
-import Logo from './logo'
+import { Github, Twitter, X, Copyright } from "lucide-react";
+import Head from "next/head";
+import Link from "next/link";
+import React from "react";
+import Logo from "./logo";
 
 export default function Header() {
     return (
@@ -10,25 +10,28 @@ export default function Header() {
             <header className="w-full border-b sticky top-0 bg-white/40 shadow-sm backdrop-blur-md z-50">
                 <div className=" mx-auto px-4 py-4 flex justify-between items-center">
                     <Logo />
-                    <div className="flex items-center space-x-4">
+                    <div className="flex items-center space-x-4 text-sm">
                         <Link
                             target="_blank"
-                            href="https://github.com/eronred/handy-arrows">
-                            <img
-                                src="/github.svg"
-                                className="w-5 h-5 cursor-pointer"
-                            />
+                            href="https://creativecommons.org/licenses/by/4.0"
+                        >
+                            License
+                        </Link>
+                        <Link target="_blank" href="mailto:erencanarica0@gmail.com">
+                            Contact
                         </Link>
                         <Link
                             target="_blank"
-                            href="https://x.com/imeronn">
-                            <img
-                                src="/x.svg"
-                                className="w-5 h-5 cursor-pointer" />
+                            href="https://github.com/eronred/handy-arrows"
+                        >
+                            <img src="/github.svg" className="w-5 h-5 cursor-pointer" />
+                        </Link>
+                        <Link target="_blank" href="https://x.com/imeronn">
+                            <img src="/x.svg" className="w-5 h-5 cursor-pointer" />
                         </Link>
                     </div>
                 </div>
             </header>
         </>
-    )
+    );
 }
